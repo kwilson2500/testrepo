8562ce6e94d4c3731f2ffbaeef9e315e440a981a
kwill2500 <kwill2500@gmail.com>
2022-11-20 17:23:57 -0500

    added dogyearsca.js containing the code from the dog years project on code academy

---
+++ dogyearsca.js
@@ -0,0 +1,10 @@
+// Dog Years - codeacademy
+
+var myAge = 25;
+var earlyYears = 2;
+earlyYears = earlyYears * 10.5;
+var laterYears = myAge - 2;
+laterYears = laterYears * 4;
+var myAgeInDogYears = earlyYears + laterYears;
+var myName = 'Kraig'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old i human years which is ${myAgeInDogYears} years old in dog years.`);
