92cf3d340b9759ad41ee2defd66c4c8915c9b40a
kwill2500 <kwill2500@gmail.com>
2022-11-20 17:10:46 -0500

    added the code for converting kelvin, to fahrenheit

--- kelvinweatherca.js
+++ kelvinweatherca.js
@@ -1,1 +1,11 @@
 // Kelvin Weather - code academy
+
+// create a variable name kelvin and assign it 293
+var kelvin = 0;
+// create a variable celsius that subtracts kelvin by 273
+var celsius = kelvin - 273;
+// create a variable fahrenheit 
+var fahrenheit = celsius * (9 / 5) + 32;
+// make the number round to the nearest whole number for fahrenheit
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
